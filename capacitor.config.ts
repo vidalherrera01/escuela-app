48ebbfe8ab62ef16af5352b658cd8f219b28ee47
vidalherrera01 <vidalherrera01@gmail.com>
2024-11-15 11:22:32 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'v_provatoria',
+  webDir: 'www'
+};
+
+export default config;
